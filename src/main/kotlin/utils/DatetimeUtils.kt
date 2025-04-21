260fe04204669378bfeb3b5d3e430d0203a35751
minhalee <minha1002@gmail.com>
2025-04-21 15:42:46 +0900

    feat: Timezone to YYYY-mm-DD

---
+++ src/main/kotlin/utils/DatetimeUtils.kt
@@ -0,0 +1,18 @@
+package utils
+
+import java.time.ZonedDateTime
+import java.time.format.DateTimeFormatter
+import java.util.*
+
+object DateTimeUtils {
+    private val pubDateFormatter = DateTimeFormatter.ofPattern("EEE, dd MMM yyyy HH:mm:ss Z", Locale.ENGLISH)
+    private val outputFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd")
+
+    fun convertPubDateToLocalDateString(pubDate: String): String {
+        // GMT → +0000 으로 치환
+        val correctedDate = pubDate.replace("GMT", "+0000")
+        val zonedDateTime = ZonedDateTime.parse(correctedDate, pubDateFormatter)
+        val localDateTime = zonedDateTime.toLocalDateTime()
+        return localDateTime.format(outputFormatter)
+    }
+}
