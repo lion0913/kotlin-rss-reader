67ecd82fb998b2546456b1ff9e66f344fbb00bf9
minhalee <minha1002@gmail.com>
2025-04-22 13:39:17 +0900

    feat: 테스트 케이스 작성

--- src/test/kotlin/ApplicationKtTest.kt
+++ src/test/kotlin/ApplicationKtTest.kt
@@ -1,17 +1,18 @@
-import org.junit.jupiter.api.AfterEach
-import org.junit.jupiter.api.BeforeEach
+import kotlinx.coroutines.runBlocking
+import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.api.Test
+import utils.BlogRss
+import utils.parseRss
 
 class ApplicationKtTest {
-    @BeforeEach
-    fun setUp() {
-    }
-
-    @AfterEach
-    fun tearDown() {
-    }
-
     @Test
-    fun main() {
-    }
+    fun `RSS 파싱이 정상적으로 동작해야 한다`() =
+        runBlocking {
+            val url = "https://tech.kakao.com/feed/"
+            val result = BlogRss.KAKAO.parseRss(url)
+
+            assertTrue(result.isNotEmpty())
+            assertTrue(result.all { it.title.isNotBlank() })
+            assertTrue(result.all { it.link.startsWith("http") })
+        }
 }
