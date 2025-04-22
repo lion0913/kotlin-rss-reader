90f80f38332380b95b6d6db2efc5267132f88482
dajung <ekwjd990913@gmail.com>
2025-04-22 13:49:45 +0900

    [test] parsePubdate 테스트코드 작성

--- src/test/kotlin/ApplicationKtTest.kt
+++ src/test/kotlin/ApplicationKtTest.kt
@@ -2,7 +2,9 @@ import kotlinx.coroutines.runBlocking
 import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.api.Test
 import utils.BlogRss
+import utils.DateTimeUtils
 import utils.parseRss
+import kotlin.test.assertEquals
 
 class ApplicationKtTest {
     @Test
@@ -15,4 +17,21 @@ class ApplicationKtTest {
             assertTrue(result.all { it.title.isNotBlank() })
             assertTrue(result.all { it.link.startsWith("http") })
         }
+
+    @Test
+    fun `PubDate 파싱이 정상 동작해야 한다`() {
+        // given
+        val input = "Tue, 22 Apr 2025 04:43:23 GMT"
+
+        // when
+        val result = DateTimeUtils.parsePubDate(input)
+
+        // then
+        assertEquals(2025, result.year)
+        assertEquals(22, result.dayOfMonth)
+        assertEquals(4, result.hour)
+        assertEquals(43, result.minute)
+        assertEquals(23, result.second)
+        assertEquals("Z", result.offset.toString()) // GMT == UTC == Z
+    }
 }
